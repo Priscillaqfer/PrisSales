5be3378ed2874d17c8814671e42c3aae935d79b6
Priscillaqfer <priscillaqf@hotmail.com>
2025-03-20 00:06:44 -0300

    Criando cadastro produto

--- exec.py
+++ exec.py
@@ -1,3 +1,4 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask import Flask, render_template, request, redirect, url_for
 from supabase import create_client, Client
 import os
@@ -14,6 +15,7 @@ supabase: Client = create_client(url, key)
 
 # Iniciar Flask
 app = Flask(__name__)
+app.secret_key = 'SecretKey' 
 
 # Rota principal - Menu de navegação
 @app.route('/')
@@ -46,27 +48,43 @@ def cadastro_cliente():
     return render_template('cadastro_cliente.html')
 
 # Rota para cadastrar produto (apenas exemplo, será semelhante à de clientes)
+
 @app.route('/cadastro_produto', methods=['GET', 'POST'])
 def cadastro_produto():
     if request.method == 'POST':
+        # Verifica se os campos existem no formulário
+        if 'nome_produto' not in request.form or 'tipo_produto' not in request.form or 'preco' not in request.form:
+            return "Campos 'nome_produto', 'tipo_produto' e 'preco' são obrigatórios", 400
+
         nome_produto = request.form['nome_produto']
+        tipo_produto = request.form['tipo_produto']
         preco = request.form['preco']
-        
+
         # Inserir os dados no Supabase
         try:
             response = supabase.table('Produtos').insert({
-                "nome_produto": nome_produto,
-                "preco": preco
+                "nome": nome_produto,
+                "tipoproduto": tipo_produto,
+                "valorproduto": preco
             }).execute()
 
             if response.data:
-                return redirect(url_for('index'))
+                flash('Cadastro realizado com sucesso!', 'success')  # Mensagem de sucesso
+                return redirect(url_for('cadastro_produto'))
             else:
                 return "Erro ao cadastrar produto", 400
         except Exception as e:
-            return f"Erro ao cadastrar: {str(e)}", 500
+            flash(f'Erro ao cadastrar: {str(e)}', 'error')
+            return redirect(url_for('cadastro_produto'))  
 
     return render_template('cadastro_produto.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+@app.route('/alterar_produto', methods=['GET'])
+def alterar_produto():
+    return render_template('alterar_produto.html')
 
 # Rota para buscar CPF e retornar os dados do cliente para alteração
 @app.route('/alterar_cliente_buscarcpf', methods=['GET', 'POST'])
